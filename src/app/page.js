2bfbd2351b4cbc809ef96050e9a0c931f43ca7f6
Krish-2512 <krish12252005@gmail.com>
2025-01-02 01:01:55 +0530

    sonsors-events(without responsive)

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 
 import AlcherApp from "./_alcher-app-section/AlcherApp";
 import Merch from "./_merch-section/Merch";
+import EventSec from "./events-section/EventSec";
+import SponsorSection from "./sponsor-section/SponsorSec";
+
+
 export default function Page() {
   return (
     <div>
-      
+      <SponsorSection/>
+      <EventSec/>
       {/* <Merch /> */}
       <AlcherApp />
+      
     </div>
   );
 }
