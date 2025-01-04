9683678bf26e3758c0eb7125cb2d3524e51e585e
Pavan Lokesh <pavanlokesh.developer@gmail.com>
2025-01-05 00:47:48 +0530

    home route Merch page and Responsiveness done

---
+++ src/app/merch/Model3D.js
@@ -0,0 +1,14 @@
+
+
+import { Canvas } from '@react-three/fiber';
+import './merch-route.css'
+
+const Model3D = ()=>{
+    return (
+        <div className="model3d">
+                
+        </div>
+    )
+}
+
+export default Model3D;
---
+++ src/app/merch/page.js
@@ -0,0 +1,10 @@
+
+// import Model3D from "./model3D";
+
+export default function Page() {
+    return (
+      <div>
+        <Model3D />
+      </div>
+    );
+  }
